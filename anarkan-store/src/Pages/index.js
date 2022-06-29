2a134d4bf04a07fa2afdca8e8c54323e6027a193
TLRKiliann <philogenie@protonmail.com>
2022-06-29 20:35:15 +0200

    footer added with some style

--- anarkan-store/src/Pages/index.js
+++ anarkan-store/src/Pages/index.js
@@ -4,83 +4,111 @@ import React from 'react';
 //import "./index.scss";
 
 const Home = () => {
-
   return (
-    <div className="home--div">
-      
-      <div className="home--sec">
-        <h1>Welcome to Anarchaik !</h1>
-        <h3>Choice what you want !</h3>
+    <div className="main--home">
+      <div className="home--div">
+        
+        <div className="home--sec">
+          <h1>Welcome to Anarchaik !</h1>
+          <h3>Choice what you want !</h3>
+        </div>
+
+        <div className="home--third">
+          <article>
+            <h3>History</h3>
+            <p>
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            </p>
+            <p>
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !<br />
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            </p>
+
+          </article>
+        </div>
+
+        <div className="home--third">
+          <article>
+            <h3>Our Work</h3>
+            <p>
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you
+            </p>
+            <p>
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you<br />
+            Last week we found special features for you !
+            Last week we found special features for you !
+            </p>
+
+          </article>
+        </div>
+
+        <div className="home--third">
+          <article>
+            <h3>Yes we can !</h3>
+            <p>
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            </p>
+            <p>
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !
+            Last week we found special features for you !<br />
+            Last week we found special features for you !
+            </p>
+
+          </article>
+        </div>
+
       </div>
 
-      <div className="home--third">
-        <article>
-          <h3>Special truc</h3>
-          <p>
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          </p>
-          <p>
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !<br />
-          Last week we found special features for you !
-          </p>
+      <footer>
+        <nav id="footer--nav">
+          <ul>
+            <li>
+              <a href="#">
+                Panier (PayPal)
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Photo-Boutique
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Ours Partners
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
 
-        </article>
-      </div>
-
-      <div className="home--third">
-        <article>
-          <h3>Special truc</h3>
-          <p>
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          </p>
-          <p>
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !<br />
-          Last week we found special features for you !
-          </p>
-
-        </article>
-      </div>
-
-      <div className="home--third">
-        <article>
-          <h3>Special truc</h3>
-          <p>
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          </p>
-          <p>
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !
-          Last week we found special features for you !<br />
-          Last week we found special features for you !
-          </p>
-
-        </article>
-      </div>
 
     </div>
   );
