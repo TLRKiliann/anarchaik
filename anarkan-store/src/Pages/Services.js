c43ab9238e1b4eafdc6dd316ce2895aabea98a09
TLRKiliann <philogenie@protonmail.com>
2022-06-30 08:27:40 +0200

    Services page at work

--- anarkan-store/src/Pages/Services.js
+++ anarkan-store/src/Pages/Services.js
@@ -3,8 +3,14 @@ import './style.css';
 
 const Services = () => {
   return (
-    <div className="mainserv--div">
-      Services
+    <div className="main--home">
+      <div className="home--first">
+        
+        <div className="home--sec">
+          <h1>Services</h1>
+          <h3>Let your dreams come true</h3>
+        </div>
+      </div>
     </div>
   );
 };
